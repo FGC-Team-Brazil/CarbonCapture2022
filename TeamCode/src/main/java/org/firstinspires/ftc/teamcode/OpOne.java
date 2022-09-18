02426c31fd84db7a2130faed7b2e074124146e06
Helena Rosa <helenarosa2005@gmail.com>
2022-09-18 12:48:54 -0300

    adding climber base code directly on main op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpOne.java
@@ -16,17 +16,30 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class OpOne extends LinearOpMode {
     private DcMotor leftMotor;
     private DcMotor rightMotor;
+    private DcMotor climbUpMotor;
+    private DcMotor climbDownMotor;
+
 
     private double leftMotorPower = 0;
     private double rightMotorPower = 0;
+    private double climbUpMotorPower = 0;
+    private double climbDownMotorPower = 0;
 
     @Override
     public void runOpMode() {
         this.leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
         this.rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
 
+        this.climbUpMotor = hardwareMap.get(DcMotor.class, "climbUpMotor"); //CORE
+        this.climbDownMotor = hardwareMap.get(DcMotor.class, "climbDownMotor"); //HEX
+
         //one side must be reversed for a drivetrain
         this.rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        //invert both climb
+        this.climbUpMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.climbDownMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -44,14 +57,24 @@ public class OpOne extends LinearOpMode {
             leftMotor.setPower(y + x);
             rightMotor.setPower(y - x);
 
+            double yClimb= gamepad2.left_stick_y;
+
+            if(yClimb > 0){
+                climbUpMotor.setPower(yClimb); //CORE
+            }else if(yClimb < 0){
+                climbDownMotor.setPower(-yClimb); //HEX
+            }
+
             //this.leftMotorPower = this.gamepad1.left_stick_y;
             //this.rightMotorPower = this.gamepad1.right_stick_y;
-
 
             telemetry.addData("Left Encoder Value", this.leftMotor.getCurrentPosition());
             telemetry.addData("Right Encoder Value", this.leftMotor.getCurrentPosition());
             telemetry.addData("Status", "Running");
             telemetry.update();
+
+
+
 
         }
     }
